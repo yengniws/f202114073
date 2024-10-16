3a99a935fdb5efe8ae427afe02a8354fb4616df6
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-10-16 09:34:43 +0900

    five

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("박예은");
     World.run();
+    World.run2();
   }
 
 }
--- World.java
+++ World.java
@@ -3,6 +3,9 @@ public class World {
     public static void run() {
       System.out.println("202114073");
     }
+    public static void run2() {
+        System.out.println("hello world");
+      }    
   
   }
   
